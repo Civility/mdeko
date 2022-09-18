46c94b77f304e468050f009e43fbfa3e842b3900
o.orlov <o.orlov@devex.pro>
2022-09-18 21:35:55 +0300

    remove garbage  nuxt conf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,26 +4,10 @@ const isDev = process.env.NODE_ENV !== 'production'
 export default defineNuxtConfig({
 	telemetry: false,
 	meta: {
-		title: 'Goods',
+		title: 'MDeko',
 		htmlAttrs: { lang: 'ru' },
 		meta: [{ name: 'viewport', content: 'width=device-width, initial-scale=1' }],
-		link: [
-			{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-			// {
-			// 	rel: 'dns-prefetch',
-			// 	href: 'https://fonts.gstatic.com/',
-			// },
-			// {
-			// 	rel: 'preconnect',
-			// 	href: 'https://fonts.gstatic.com',
-			// },
-
-			// {
-			// 	rel: 'stylesheet',
-			// 	href:
-			// 		'https://fonts.googleapis.com/css2?family=Roboto+Condensed:wght@400;700&family=Roboto+Flex:opsz,wght@8..144,300;8..144,500;8..144,700&display=swap',
-			// },
-		],
+		// link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
 	},
 	components: {
 		dirs: ['~/components/element', '~/components/partial', '~/components/shared'],
