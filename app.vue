cfe21a0b6f1c5665247d23ff23239fd9e194b65f
o.orlov <o.orlov@devex.pro>
2022-09-18 21:34:07 +0300

    add app useHead meta

--- app.vue
+++ app.vue
@@ -5,3 +5,10 @@
 	</NuxtLayout>
 	<Footer />
 </template>
+<script setup>
+const route = useRoute()
+
+useHead({
+	meta: [{ name: 'og:title', content: `akvamdeko | ${route.meta.title}` }],
+})
+</script>
