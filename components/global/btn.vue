6128579a98a11313647e382d92192dbc8018d86c
Civility <Civilitys@gmail.com>
2023-04-30 18:44:28 +0300

    btn outline-none

--- components/global/btn.vue
+++ components/global/btn.vue
@@ -43,6 +43,7 @@ const isClass = computed(() => {
 
 <style scoped lang="postcss">
 .btn {
+	appearance: none;
 	@apply inline-flex items-center justify-center;
 	@apply py-2 lg:py-3 px-2 w-auto;
 
@@ -50,9 +51,8 @@ const isClass = computed(() => {
 	@apply text-sm xl:text-lg md:text-base;
 	@apply align-middle not-italic;
 	@apply transition-all duration-300;
-	@apply focus:ring-0 focus:outline-none;
-	@apply disabled:isolate disabled:pointer-events-none;
-	@apply disabled:opacity-70;
+	@apply !outline-none target:outline-none focus:outline-none focus:ring-0 focus-visible:outline-none active:outline-none;
+	@apply disabled:pointer-events-none disabled:isolate disabled:opacity-70;
 	-webkit-appearance: none;
 	&__main {
 		/* @apply border-b-2 border-r-2 border-main-light  hover:border-main-lighter rounded-lg; */
